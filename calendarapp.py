265edc24561bdacfae2412680048c203f7f78c14
Raphael Pierzina <raphael@hackebrot.de>
2015-03-04 23:57:50 +0100

    Set the window size to emulate a mobile device

--- calendarapp.py
+++ calendarapp.py
@@ -1,3 +1,10 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.config import Config
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '640')
+
 from kivy.app import App
 
 
