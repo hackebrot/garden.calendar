9b560d4459568b322f512a7ee6b99f100063f7ab
Raphael Pierzina <raphael@hackebrot.de>
2015-03-10 00:19:39 +0100

    Implement scrollview that contains all the weeks

--- calendarapp.py
+++ calendarapp.py
@@ -6,10 +6,19 @@ Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '640')
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import BoundedNumericProperty
+
+
+class Week(BoxLayout):
+    week_num = BoundedNumericProperty(1, min=1, max=53)
 
 
 class CalendarApp(App):
     """Basic App to hold the calendar widget."""
 
     def build(self):
-        return self.root
+        scroll_view = self.root
+        for i in range(1, 54):
+            scroll_view.ids.cal.add_widget(Week(week_num=i))
+        return scroll_view
